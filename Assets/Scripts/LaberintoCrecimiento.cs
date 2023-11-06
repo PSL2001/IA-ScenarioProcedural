cf0e855ac1d77173e9c9bd20cce625132d770ad2
Pablo Sánchez López <65461723+PSL2001@users.noreply.github.com>
2023-11-06 22:04:06 +0100

    Añadido laberinto procedural

--- Assets/Scripts/LaberintoCrecimiento.cs
+++ Assets/Scripts/LaberintoCrecimiento.cs
@@ -26,13 +26,74 @@ public class LaberintoCrecimiento : MonoBehaviour
             {
                 piezaActual = Instantiate(pieza, new Vector3(x, 0, z), Quaternion.identity);
                 totalPiezas--;
-                //Crece hacia el norte
-                float newX = x;
-                float newZ = z + 5;
-                hayHueco = true;
-
-                StartCoroutine(Generar(newX, newZ));
             }
+            int sentidoCrecimiento = Random.Range(0, 4);
+            float newX = 0;
+            float newZ = 0;
+
+            RaycastHit hit;
+
+            switch (sentidoCrecimiento)
+            {
+                case 0: //Norte
+                    if (Physics.Raycast(piezaActual.transform.position, piezaActual.transform.forward, out hit, 3))
+                    {
+                        hayHueco = false;
+                        piezaActual = hit.transform.gameObject;
+                    }
+                    else
+                    {
+                        hayHueco = true;
+                    }
+                    //Crece hacia el norte
+                    newX = piezaActual.transform.position.x;
+                    newZ = piezaActual.transform.position.z + 5;
+                    break;
+                case 1: //Sur
+                    if (Physics.Raycast(piezaActual.transform.position, piezaActual.transform.forward * -1, out hit, 3))
+                    {
+                        hayHueco = false;
+                        piezaActual = hit.transform.gameObject;
+                    }
+                    else
+                    {
+                        hayHueco = true;
+                    }
+                    //Crece hacia el Sur
+                    newX = piezaActual.transform.position.x;
+                    newZ = piezaActual.transform.position.z - 5;
+                    break;
+                case 2: //Este
+                    if (Physics.Raycast(piezaActual.transform.position, piezaActual.transform.right, out hit, 3))
+                    {
+                        hayHueco = false;
+                        piezaActual = hit.transform.gameObject;
+                    }
+                    else
+                    {
+                        hayHueco = true;
+                    }
+                    //Crece hacia el Este
+                    newX = piezaActual.transform.position.x + 5;
+                    newZ = piezaActual.transform.position.z;
+                    break;
+                case 3: //Oeste
+                    if (Physics.Raycast(piezaActual.transform.position, piezaActual.transform.right * -1, out hit, 3))
+                    {
+                        hayHueco = false;
+                        piezaActual = hit.transform.gameObject;
+                    }
+                    else
+                    {
+                        hayHueco = true;
+                    }
+                    //Crece hacia el Oeste
+                    newX = piezaActual.transform.position.x - 5;
+                    newZ = piezaActual.transform.position.z;
+                    break;
+            }
+            
+            StartCoroutine(Generar(newX, newZ));
         }
     }
 }
